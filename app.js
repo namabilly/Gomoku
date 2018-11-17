30344537ab659a512dcb077a5cf8fc99ab392d9c
0Accelerator0 <948386361@qq.com>
2018-11-18 06:02:42 +0800

    Fixed port for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log('server started');
 
 app.get('/', function (req, res) {
